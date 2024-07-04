895af789cdb7489868047eca2ab54b29045cc03c
DK0K322 <103969813+DK0K322@users.noreply.github.com>
2024-07-04 22:36:26 +0200

    add script

---
+++ main.js
@@ -0,0 +1,53 @@
+function enter(){
+    var user = document.getElementById("user").value;
+    var pass = document.getElementById("pass").value;
+  
+  if(user == "v0idk" && pass == "Randompassword"){
+              publifram()
+     }
+    else if(user == "e" && pass == "goob"){
+              publifram()
+     }
+    else if(user == "e" && pass == "goob"){
+              publifram()
+     }
+    else if(user == "e" && pass == "goob"){
+              publifram()
+     }
+    else if(user == "e" && pass == "goob"){
+              publifram()
+     }
+    else if(user == "e" && pass == "goob"){
+              publifram()
+     }
+    else if(user == "e" && pass == "goob"){
+              publifram()
+     }
+    else if(user == "e" && pass == "goob"){
+              publifram()
+     }
+    else if(user == "e" && pass == "goob"){
+              publifram()
+     }
+    else if(user == "e" && pass == "goob"){
+              publifram()
+     }
+    else if(user == "e" && pass == "goob"){
+              publifram()
+     }
+    else if(user == "e" && pass == "goob"){
+              publifram()
+     }
+    else if(user == "e" && pass == "goob"){
+              publifram()
+     }
+    else if(user == "e" && pass == "goob"){
+              publifram()
+     }
+    else if(user == "e" && pass == "goob"){
+              publifram()
+     }
+    else{
+      alert("Incorrect Credentials");
+      }
+  }
