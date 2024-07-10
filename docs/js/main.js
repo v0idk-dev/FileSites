a6023e5209f59160e1108982be3b6b2128b1dfba
DK0K322 <103969813+DK0K322@users.noreply.github.com>
2024-07-10 21:10:25 +0300

    add panic users

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,53 +1,67 @@
-function enter(){
-    var user = document.getElementById("user").value;
-    var pass = document.getElementById("pass").value;
-  
-  if(user == "v0idk" && pass == "Randompassword"){
-              main()
-     }
-    else if(user == "e" && pass == "goob"){
-              main()
-     }
-    else if(user == "e" && pass == "goob"){
-              main()
-     }
-    else if(user == "e" && pass == "goob"){
-              main()
-     }
-    else if(user == "e" && pass == "goob"){
-              main()
-     }
-    else if(user == "e" && pass == "goob"){
-              main()
-     }
-    else if(user == "e" && pass == "goob"){
-              main()
-     }
-    else if(user == "e" && pass == "goob"){
-              main()
-     }
-    else if(user == "e" && pass == "goob"){
-              main()
-     }
-    else if(user == "e" && pass == "goob"){
-              main()
-     }
-    else if(user == "e" && pass == "goob"){
-              main()
-     }
-    else if(user == "e" && pass == "goob"){
-              main()
-     }
-    else if(user == "e" && pass == "goob"){
-              main()
-     }
-    else if(user == "e" && pass == "goob"){
-              main()
-     }
-    else if(user == "e" && pass == "goob"){
-              main()
-     }
-    else{
-      splashError()
-      }
+function enter() {
+  var user = document.getElementById("user").value;
+  var pass = document.getElementById("pass").value;
+
+ 
+  if (user == "Oughzhadhalhous Minsodis" && pass == "ProMaxUltra") {
+    main()
   }
+
+  // Panic Users
+  else if (user == "asdf") {
+    splashError()
+  }
+  else if (user == "asd") {
+    splashError()
+  }
+  else if (user == "sadf") {
+    splashError()
+  }
+  else if (user == "") {
+    splashError()
+  }
+  else if (user == "") {
+    splashError()
+  }
+
+  // System Users
+  else if (user == "del") {
+    del()
+  }
+  else if (user == "troll") {
+    troll()
+  }
+
+  // Normal Users
+  else if (user == "e" && pass == "goob") {
+    main()
+  }
+  else if (user == "e" && pass == "goob") {
+    main()
+  }
+  else if (user == "e" && pass == "goob") {
+    main()
+  }
+  else if (user == "e" && pass == "goob") {
+    main()
+  }
+  else if (user == "e" && pass == "goob") {
+    main()
+  }
+  else if (user == "e" && pass == "goob") {
+    main()
+  }
+  else if (user == "e" && pass == "goob") {
+    main()
+  }
+  else if (user == "e" && pass == "goob") {
+    main()
+  }
+  else {
+    splashError()
+  }
+}
+
+function wait(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
